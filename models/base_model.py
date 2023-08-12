b457ccc0c3a5ce410c386ea393b616b4c8dd8379
Blessing Udeji <budeji@gmail.com>
2023-08-12 20:05:26 +0100

    Basemodel

---
+++ models/base_model.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+from uuid import uuid4
+from datetime import datetime
+
+
+class BaseModel():
+    """Base class for models"""
+
+    def __init__(self, *args, **kwargs):
+        """Initializes the BaseModel class."""
+
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == 'created_at ' or key == 'updated_at':
+                    time_f = '%Y-%m-%dT%H:%M:%S.%f'
+                    value = datetime.strptime(value, time_f)
+                if key != '__class__':
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid4)
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+
+    def __str__(self):
+        """String representation of the instance."""
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__})]"
+
+    def save(self):
+        """Update the 'updated_at' attribute"""
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """Converts the instance to a dictionary representation."""
+        dictionary = self.__dict__.copy()
+        dictionary['__class__'] = self.__class__.__name__
+        dictionary['created_at'] = self.created_at.isoformat()
+        dictionary['updated_at'] = self.updated_at.isoformat()
+
+        return dictionary

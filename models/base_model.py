b86375b50a08cad584c6a8aa18e46efe0bee881d
Blessing Udeji <budeji@gmail.com>
2023-08-12 20:23:22 +0100

    Basemodel

--- models/base_model.py
+++ models/base_model.py
@@ -17,7 +17,7 @@ class BaseModel():
                 if key != '__class__':
                     setattr(self, key, value)
         else:
-            self.id = str(uuid4)
+            self.id = str(uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
 
